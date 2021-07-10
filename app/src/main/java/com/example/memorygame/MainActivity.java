88f50a6bb9e46ce1646906cd4abcebf7df326328
truongkimson <60567409+truongkimson@users.noreply.github.com>
2021-07-10 13:50:48 +0800

    add app icon and launch splash

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -8,7 +8,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_AppCompat_Light_NoActionBar);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_2);
+        setContentView(R.layout.activity_main);
     }
 }
