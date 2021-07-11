ee3cd5ab06de97e78e8c0f5ccec963425df92ef5
Shirley <chowjss@gmail.com>
2021-07-11 08:59:44 +0800

    Revert "URL validator added. Async tasks handles websites with less than 20 images."
    This reverts commit 88b492cd5f4dd7c3ece5ea05a0ded5e4f8a1a70d.

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -1,29 +1,33 @@
 package com.example.memorygame;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InterruptedIOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     Content myContent;
-    ImageButton [] buttons;
-    Bitmap [] bitmap;
+    ImageButton buttons[];
+    Bitmap bitmap[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,21 +44,12 @@ public class MainActivity extends AppCompatActivity {
                 myContent=new Content();
                 EditText enteredURL=findViewById(R.id.editTextURL);
                 String URL=enteredURL.getText().toString();
-                if (URLUtil.isValidUrl(URL))
-                    myContent.execute(URL);
+                myContent.execute(URL);
             }
         });
     }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (myContent!=null) {
-            myContent.cancel(true);
-        }
-    }
     public class Content extends AsyncTask<String, Void, Void> {
         private AsyncTask<String, Void, Void> updateTask = null;
-        private Boolean flag=true;
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -64,24 +59,20 @@ public class MainActivity extends AppCompatActivity {
             try {
                 //Connect to the website
                 Document document = Jsoup.connect(Strings[0]).get();
+
                 //Get the logo source of the website
                 Elements images = document.select("img[src$=.jpg]");
-                if (images.size()<20)
-                    flag=false;
-                else
-                {   // Locate the src attribute
-                    for (int i=0;i<20;i++)
-                    {
-                        if (isCancelled())
-                            break;
-                        String imgSrc = images.get(i).absUrl("src");
-                        // Download image from URL
-                        InputStream input = new java.net.URL(imgSrc).openStream();
-                        // Decode Bitmap
-                        bitmap[i] = BitmapFactory.decodeStream(input);
-                    }
+                // Locate the src attribute
+                for (int i=0;i<20;i++)
+                {
+                    if (isCancelled())
+                        break;
+                    String imgSrc = images.get(i).absUrl("src");
+                    // Download image from URL
+                    InputStream input = new java.net.URL(imgSrc).openStream();
+                    // Decode Bitmap
+                    bitmap[i] = BitmapFactory.decodeStream(input);
                 }
-
 
 
             } catch (IOException e) {
@@ -93,19 +84,11 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-            if (flag)
-            {
-                for (int j = 0; j < 20; j++) {
-                    String ImageButtonName = "button" + (j + 1);
-                    int resIDImageButton = getResources().getIdentifier(ImageButtonName, "id", getPackageName());
-                    buttons[j]=findViewById(resIDImageButton);
-                    buttons[j].setImageBitmap(bitmap[j]);
-                }
-            }
-            else
-            {
-                Toast toast=Toast.makeText(getApplicationContext(),"Insufficient images for game",Toast.LENGTH_SHORT);
-                toast.show();
+            for (int j = 0; j < 20; j++) {
+                String ImageButtonName = "button" + (j + 1);
+                int resIDImageButton = getResources().getIdentifier(ImageButtonName, "id", getPackageName());
+                buttons[j]=findViewById(resIDImageButton);
+                buttons[j].setImageBitmap(bitmap[j]);
             }
         }
     }
