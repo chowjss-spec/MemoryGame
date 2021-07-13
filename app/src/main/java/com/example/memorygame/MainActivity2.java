725bab68d026d216bc6483821d1c54bd85e89940
ZhengguangHan <hanzh93@gmail.com>
2021-07-13 15:01:33 +0800

    Added Delay to revert back to covered after 6 seconds

--- app/src/main/java/com/example/memorygame/MainActivity2.java
+++ app/src/main/java/com/example/memorygame/MainActivity2.java
@@ -29,7 +29,7 @@ public class MainActivity2 extends AppCompatActivity {
 
     int matchComplete =0;
 
-    private static final long startTimeInMil = 60000;
+    private static final long startTimeInMil = 6000;
 
     private TextView mTextViewCountDown;
     private CountDownTimer mCountDownTimer;
@@ -37,6 +37,32 @@ public class MainActivity2 extends AppCompatActivity {
     private long mTimeLeftInMillis = startTimeInMil;
     private SoundEffect sound;
     private Handler handler = new Handler();
+
+    private void startTimer(ArrayList<ImageButton> tempClicked){
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis,1000) {
+            @Override
+            public void onTick(long l) {
+                mTimeLeftInMillis= l;
+            }
+
+            @Override
+            public void onFinish() {
+
+                for (ImageButton buttons : tempClicked) {
+                    buttons.setImageResource(R.drawable.x);
+                }
+                System.out.println("Don't match");
+
+                tempClicked.clear();
+                mTimeLeftInMillis=startTimeInMil;
+
+            }
+        }.start();
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +73,7 @@ public class MainActivity2 extends AppCompatActivity {
 
         //For timer
         mTextViewCountDown = findViewById(R.id.timer);
+
 
         //For Chronometer
         Chronometer chronoTimer = (Chronometer) findViewById(R.id.chronoTimer);
@@ -168,6 +195,7 @@ public class MainActivity2 extends AppCompatActivity {
                                     else {
                                         sound.incorrectMatch();
                                         tempClicked.add(button);
+                                        startTimer((ArrayList<ImageButton>) tempClicked);
                                     }
                                 }
                             }
@@ -181,6 +209,7 @@ public class MainActivity2 extends AppCompatActivity {
                                 buttons.setImageResource(R.drawable.x);
                             }
                             System.out.println("Don't match");
+
                             tempClicked.clear();
                         }
 
